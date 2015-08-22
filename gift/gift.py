4a0d5cb3cb2cbea914c36498e1d49c7cc04c248c
Misha Wolfson <misha@ginkgobioworks.com>
2015-08-22 11:54:19 -0400

    Port in HTML5 boilerplate

--- gift/gift.py
+++ gift/gift.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, url_for
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+  return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+  return redirect(url_for('static', filename='favicon.ico'))
+
 
 if __name__ == '__main__':
-    app.run()
+  app.run(debug=True)
